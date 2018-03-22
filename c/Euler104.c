77dfc7b587d04a0aa35273cedfd45e215ef0cc66
raikuma <whdnddh@naver.com>
2018-03-22 23:08:40 +0900

    Clear 104

--- c/Euler104.c
+++ c/Euler104.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <gmp.h>
 
 int isPanDigital(char *s) {
@@ -15,6 +16,41 @@ int isPanDigital(char *s) {
     return 1;
 }
 
+int isDoublePD(char *s) {
+    int l = strlen(s);
+    if (isPanDigital(s) && isPanDigital(s+l-9)) {
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
+    mpz_t n1, n2;
+    mpz_t *f1 = &n1, *f2 = &n2, *t;
+    char *str;
+    int i;
+    int k;
+
+    mpz_init(n1);
+    mpz_init(n2);
+    mpz_set_ui(n1, 1);
+    mpz_set_ui(n2, 1);
+    k = 2;
+
+    while (1) {
+        mpz_add(*f1, *f1, *f2);
+        k += 1;
+        str = mpz_get_str(NULL, 10, *f1);
+        if (isDoublePD(str)) {
+            printf("%s %d\n", str, k);
+            break;
+        }
+        t = f1;
+        f1 = f2;
+        f2 = t;
+    }
+
+    mpz_clear(n1);
+    mpz_clear(n2);
     return 0;
 }
